e400132dca659a4cb70d73757687ee2a3b682870
Trisha Atluri <trishaatluri@Trishas-MacBook-Pro.local>
2019-08-14 19:48:00 -0400

    Updates

---
+++ precache-manifest.2233fc74a2ab3cd6e2152291e738eb7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b3f25b8283c97a8cf4d09bc420f4a8",
+    "url": "/dnadatastorageweb/index.html"
+  },
+  {
+    "revision": "fabe21e7c1d4dfcd175e",
+    "url": "/dnadatastorageweb/static/css/main.35236fc1.chunk.css"
+  },
+  {
+    "revision": "e55f545497cb45ca726c",
+    "url": "/dnadatastorageweb/static/js/2.b5c476bc.chunk.js"
+  },
+  {
+    "revision": "fabe21e7c1d4dfcd175e",
+    "url": "/dnadatastorageweb/static/js/main.d145382a.chunk.js"
+  },
+  {
+    "revision": "4b0c1577b2ae1a180373",
+    "url": "/dnadatastorageweb/static/js/runtime~main.31509515.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dnadatastorageweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
